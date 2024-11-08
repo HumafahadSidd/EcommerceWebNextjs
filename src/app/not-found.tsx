4f936ab1387642d4e3adac976338d4f2e215f593
HumafahadSidde <fhuma302@gmail.com>
2024-11-08 23:53:20 +0500

    sec commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Notfound() {
+  return (
+    <div className='h-screen bg-slate-400'>
+      <h1 className='ml-44 mt-30'>Oops an error occured !</h1>
+    </div>
+  )
+}
